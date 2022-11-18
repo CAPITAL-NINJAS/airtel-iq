24c3f38c7135e7762db0252223b95bf699809291
Atharva2502 <natharvanitin4@gmail.com>
2022-11-18 12:50:05 +0530

    Auth removed

--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -1,36 +1,12 @@
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 
 exports.postCallback = catchAsync(async (req, res, next) => {
-  let authHeader;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Basic')
-  ) {
-    authHeader = req.headers.authorization.split(' ')[1];
-  }
+  const data = req.body;
 
-  if (!authHeader) {
-    res.setHeader('WWW-Authenticate', 'Basic');
-    return next(new AppError('You are not authenticated!', 401));
-  }
+  console.log(data);
 
-  let auth = new Buffer.from(authHeader, 'base64').toString().split(':');
-
-  const key = auth[0];
-  const secret = auth[1];
-
-  if (key == process.env.API_KEY && secret == process.env.API_SECRET) {
-    const data = req.body;
-
-    console.log(data);
-
-    res.status(200).json({
-      status: 'success',
-      data,
-    });
-  } else {
-    res.setHeader('WWW-Authenticate', 'Basic');
-    return next(new AppError('You are not authenticated!', 401));
-  }
+  res.status(200).json({
+    status: 'success',
+    data,
+  });
 });
