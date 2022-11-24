49182745ae1853ba7fdf07115f63a3794ebc4fc9
Atharva2502 <natharvanitin4@gmail.com>
2022-11-24 21:16:08 +0530

    Making Schemas

---
+++ models/customerModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const customerSchema = new mongoose.Schema({
+  first_name: {
+    type: String,
+    trim: true,
+  },
+  last_name: {
+    type: String,
+    trim: true,
+  },
+  address: {
+    country: {
+      type: String,
+    },
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email address'],
+  },
+  photo: {
+    type: String,
+    default: 'default.jpg',
+  },
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+module.exports = Customer;
