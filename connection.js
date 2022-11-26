3a588dcbe3129bba0371c3a2e84eb9eb6b8a7283
Atharva2502 <natharvanitin4@gmail.com>
2022-11-26 21:33:06 +0530

    added connection.js file

---
+++ connection.js
@@ -0,0 +1,3 @@
+exports.getData = (data) => {
+  console.log(data);
+};
--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -1,9 +1,10 @@
 const catchAsync = require('../utils/catchAsync');
+const { getData } = require('./../connection');
 
 exports.postCallback = catchAsync(async (req, res, next) => {
   const data = req.body;
 
-  console.log(data);
+  getData(data);
 
   res.status(200).json({
     status: 'success',
