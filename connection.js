0dc36093bd65e1e80f32cb6685b4d922028ef925
Atharva2502 <natharvanitin4@gmail.com>
2022-11-26 22:16:09 +0530

    removed axios error

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const whatsapp = require('./whatsapp/whatsapp');
 
 const connectRasa = async (options) => {
   const response = await axios.post(
-    'http://localhost:5005/webhooks/rest/webhook',
+    'http://127.0.0.1:5005/webhooks/rest/webhook',
     options
   );
 
