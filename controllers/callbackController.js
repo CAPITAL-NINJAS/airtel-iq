9e8f56f3241e0647a8c688cb336bbfcb9d9f8886
Atharva2502 <natharvanitin4@gmail.com>
2022-11-26 22:56:46 +0530

    debugging... - 1

--- controllers/callbackController.js
+++ controllers/callbackController.js
@@ -5,9 +5,10 @@ exports.postCallback = catchAsync(async (req, res, next) => {
   const data = req.body;
 
   console.log(data);
-  console.log(data.message.text.body);
 
-  // getData(data);
+  if (data != {}) {
+    getData(data);
+  }
 
   res.status(200).json({
     status: 'success',
