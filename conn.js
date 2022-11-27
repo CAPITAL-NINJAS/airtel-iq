b65182b4b43660ace5ad95722825d777f7186f03
Atharva2502 <natharvanitin4@gmail.com>
2022-11-27 15:42:25 +0530

    added till services

--- conn.js
+++ conn.js
@@ -7,9 +7,22 @@ exports.getData = async (data) => {
   const message = data.message;
 
   if (sessionId && fromMob && toMob && message) {
+    const res = { sessionId, from: toMob, to: fromMob, message };
+
     if (message.text.body == 'Hii') {
-      const res = { sessionId, from: toMob, to: fromMob, message };
       sendWelcome(res);
+    } else if (
+      message.interactive.button_reply &&
+      message.interactive.button_reply.title == 'English'
+    ) {
+      sendEnglishReply(res);
+      sendServices(res);
+    } else if (
+      message.interactive.button_reply &&
+      message.interactive.button_reply.title == 'Hindi'
+    ) {
+      sendHindiReply(res);
+      sendServices(res);
     }
   }
 };
@@ -40,4 +53,56 @@ const sendWelcome = (options) => {
   options.buttons = buttons;
 
   whatsapp.sendInteractiveBtn(options);
+};
+
+const sendEnglishReply = (options) => {
+  const message = {
+    text: `Great how may I help you`,
+  };
+
+  options.message = message;
+  whatsapp.sendOneText(options);
+};
+
+const sendHindiReply = (options) => {
+  const message = {
+    text: `Sorry, currently I am not trained on Hindi\n
+            let's continue in English`,
+  };
+
+  options.message = message;
+  whatsapp.sendOneText(options);
+};
+
+const sendServices = (options) => {
+  const message = {
+    text: 'Here are the services provided by me',
+  };
+
+  const list = {
+    heading: 'Services',
+    options: [
+      {
+        tag: 'Banking ',
+        title: 'Banking Services',
+        description: 'Check account balance | Get mini statement & more',
+      },
+      {
+        tag: 'Finance',
+        title: 'Financial Services',
+        description: 'Wealth management | Financial advisory & more',
+      },
+      {
+        tag: 'Insurance',
+        title: 'Insurance Services',
+        description:
+          'Buy | Renew | Get details | Terminate | Installment & more',
+      },
+    ],
+  };
+
+  options.message = message;
+  options.list = list;
+
+  whatsapp.sendInteractiveList(options);
 };
